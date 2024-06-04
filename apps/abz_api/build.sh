0863fd628de852521c25c93440b21cfa7c2c7da1
gallaguz <gallaguz@gmail.com>
2024-06-04 11:12:34 +0300

    ABZ cleanup unnecessary files

--- apps/abz_api/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -f Dockerfile -t abz_api:latest "$(pwd)"
-docker tag abz_api:latest localhost:5000/abz_api:latest
-docker push localhost:5000/abz_api:latest
-
-docker build --no-cache -f Dockerfile.seed -t abz_seed:latest "$(pwd)"
-docker tag abz_seed:latest localhost:5000/abz_seed:latest
-docker push localhost:5000/abz_seed:latest
--- apps/abz_converter/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-docker build --no-cache -f Dockerfile -t abz_converter:latest "$(pwd)"
-docker tag abz_converter:latest localhost:5000/abz_converter:latest
-docker push localhost:5000/abz_converter:latest
